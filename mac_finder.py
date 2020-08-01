c7b87b63b6081a7bc36ae0bbedb10343008260dc
Tsega41 <tsega207@gmail.com>
2020-08-01 14:39:49 +0300

    Adding helper comments on the functions

--- mac_finder.py
+++ mac_finder.py
@@ -1,10 +1,16 @@
 """
 Creator is Tsega Amanuel
+Be sure to have scapy installed
 """
 from scapy.all import *
 from time import sleep
 
 def mac_finder(ip):
+    """
+    Sends an ARP packet broadcasting who has
+    the given ip and returns the Mac adress of
+    the given ip
+    """
     packet = ARP(pdst=ip)
     broadcast = Ether(src="1c:7f:2c:15:68:27")
     broadcast_packet = broadcast / packet 
@@ -12,6 +18,14 @@ def mac_finder(ip):
     return answered.dst
 
 def scan_everything():
+    """
+    Sends the whole network an ARP packet
+    requesting the Mac adress of all the devices 
+    in the entire subnet
+
+    NOTE if the ip does not appear it means it is
+    note being used
+    """
     x = 0
     while x <= 255:
         packet = ARP(pdst="192.168.1." + str(x))
