6547c88e08a8ef18ecdea324203c6a1d1cfb0e0c
Tsega41 <tsega207@gmail.com>
2020-08-02 16:25:44 +0300

    Fixing more bugs

--- mac_finder.py
+++ mac_finder.py
@@ -5,6 +5,7 @@ Be sure to have scapy installed
 from scapy.all import *
 from time import sleep
 
+
 def mac_finder(ip):
     """
     Sends an ARP packet broadcasting who has
@@ -17,24 +18,25 @@ def mac_finder(ip):
     answered = send(broadcast_packet, verbose = False, return_packets= True)[0]
     return answered.dst
 
-def scan_everything():
+def scan_everything(router_ip):
     """
     Sends the whole network an ARP packet
     requesting the Mac adress of all the devices 
-    in the entire subnet
+    in the entire subnet.
 
-    NOTE if the ip does not appear it means it is
-    not being used
+
+
+    NOTE:---    ENTER THE FIRST THREE LAYERS OF THE ROUTER'S IP ADDRESS
+
     """
     x = 0
     while x <= 255:
-        packet = ARP(pdst="192.168.1." + str(x))
+        packet = ARP(pdst = router_ip + "." + str(x))
         broadcast = Ether()
         broadcast_packet = broadcast / packet 
         answered = send(broadcast_packet, verbose = False, return_packets= True)[0]
-        if answered.dst != "ff:ff:ff:ff:ff:ff":
-            print("\nIP:\n" + str(answered.pdst) + "\n\nMAC:\n" + str(answered.dst) + "\n--------------------")
-            sleep(1)
+        print("\nIP:\n" + str(answered.pdst) + "\n\nMAC:\n" + str(answered.dst) + "\n--------------------")
+        sleep(1)
         x += 1
 
-
+scan_everything("192.168.0")
